9c1c55fed2a11dfa315044fa6aba99415f0ff256
Moise <nsingi-moise@hotmail.com>
2021-05-10 22:44:32 +0200

    feat(form): add login page form

---
+++ pages/forms/login.tsx
@@ -0,0 +1,56 @@
+import styles from "../../styles/forms/Register.module.scss";
+import loginStyles from "../../styles/forms/Login.module.scss";
+import Link from "next/link";
+
+
+export default function Login() {
+
+    return (
+        <>
+            <section className={loginStyles.form_container}>
+                <section className={loginStyles.container}>
+                    <section className={loginStyles.moved_element}>
+                        <section className={loginStyles.form_register_left_login}>
+                            <div className={loginStyles.form_register_left_content}>
+                                <h1>Social-Unifier</h1>
+                                <h3>Welcome to register page</h3>
+                            </div>
+                        </section>
+                        <section className={loginStyles.form_register_right_login}>
+                            <div className={loginStyles.form_container_right}>
+                                <div className={loginStyles.form_register_title}>
+                                    <h2>Login</h2>
+                                </div>
+
+                                <div className={loginStyles.form_register_text}>
+                                    <h4>Login to your account</h4>
+                                </div>
+
+                                <form action="" className={loginStyles.form_register}>
+                                    <div className={loginStyles.form_group}>
+                                        <label htmlFor="#">Username / E-mail</label>
+                                        <input type="text"/>
+                                    </div>
+                                    <div className={loginStyles.form_group}>
+                                        <label htmlFor="#">Password</label>
+                                        <input type="password"/>
+                                    </div>
+
+                                    <button type="submit" form={loginStyles.form_register}>Sign in</button>
+                                </form>
+
+                                <p className={loginStyles.exist_account}>Don't have an account ?
+                                    <span>
+                                    <Link href="#">
+                                        <a>Join us now</a>
+                                    </Link>
+                                </span>
+                                </p>
+                            </div>
+                        </section>
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+}
--- pages/forms/register.tsx
+++ pages/forms/register.tsx
@@ -7,46 +7,48 @@ export default function Register() {
         <>
             <section className={styles.form_container}>
                 <section className={styles.container}>
-                    <section className={styles.form_register_left}>
-                        <div className={styles.form_register_left_content}>
-                            <h1>Social-Unifier</h1>
-                            <h3>Welcome to register page</h3>
-                        </div>
-                    </section>
-                    <section className={styles.form_register_right}>
-                        <div className={styles.form_container_right}>
-                            <div className={styles.form_register_title}>
-                                <h2>Register</h2>
+                    <section className={styles.moved_element}>
+                        <section className={styles.form_register_left}>
+                            <div className={styles.form_register_left_content}>
+                                <h1>Social-Unifier</h1>
+                                <h3>Welcome to register page</h3>
                             </div>
+                        </section>
+                        <section className={styles.form_register_right}>
+                            <div className={styles.form_container_right}>
+                                <div className={styles.form_register_title}>
+                                    <h2>Register</h2>
+                                </div>
 
-                            <form action="" id={styles.form_register}>
-                                <div className={styles.form_group}>
-                                    <label htmlFor="#">Full name</label>
-                                    <input type="text"/>
-                                </div>
-                                <div className={styles.form_group}>
-                                    <label htmlFor="#">Email</label>
-                                    <input type="text"/>
-                                </div>
-                                <div className={styles.form_group}>
-                                    <label htmlFor="#">Password</label>
-                                    <input type="text"/>
-                                </div>
-                                <div className={styles.form_group}>
-                                    <label htmlFor="#">Confirm password</label>
-                                    <input type="text"/>
-                                </div>
-                            </form>
+                                <form action="" id={styles.form_register}>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="#">Full name</label>
+                                        <input type="text"/>
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="#">Email</label>
+                                        <input type="email"/>
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="#">Password</label>
+                                        <input type="password"/>
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="#">Confirm password</label>
+                                        <input type="password"/>
+                                    </div>
+                                </form>
 
-                            <button type="submit" form={styles.form_register}>Create Account</button>
-                            <p className={styles.exist_account}>Already have an account ?
-                                <span>
+                                <button type="submit" form={styles.form_register}>Create Account</button>
+                                <p className={styles.exist_account}>Already have an account ?
+                                    <span>
                                     <Link href="#">
                                         <a>Log in</a>
                                     </Link>
                                 </span>
-                            </p>
-                        </div>
+                                </p>
+                            </div>
+                        </section>
                     </section>
                 </section>
             </section>
